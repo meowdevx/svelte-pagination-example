ec25811c05ef3966d337e7ee679b4fc0e47309c4
Xiaoming Jia <git@meow.dev>
2021-11-27 19:27:53 -0800

    reformat to use spaces.

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,12 +3,12 @@ import type { LoadInput } from '@sveltejs/kit';
 const perPage = 10;
 
 export type PostStats = {
-	maxPages: number;
-	perPage: number;
+  maxPages: number;
+  perPage: number;
 };
 export type Post = {
-	title: string;
-	body: string;
+  title: string;
+  body: string;
 };
 
 type Fetch = LoadInput['fetch'];
@@ -16,20 +16,20 @@ type Fetch = LoadInput['fetch'];
 // In a real app, this can be implemented by listing static files
 // or querying a database to find the maximum number of posts.
 export async function getPostStats(fetch: Fetch): Promise<PostStats> {
-	const posts: Array<Post> = await fetch('https://jsonplaceholder.typicode.com/posts').then((r) =>
-		r.json()
-	);
-	const maxPages = Math.ceil(posts.length / perPage);
-	return {
-		maxPages,
-		perPage
-	};
+  const posts: Array<Post> = await fetch('https://jsonplaceholder.typicode.com/posts').then((r) =>
+    r.json()
+  );
+  const maxPages = Math.ceil(posts.length / perPage);
+  return {
+    maxPages,
+    perPage
+  };
 }
 
 export async function getPage(page: number, fetch: Fetch): Promise<Array<Post>> {
-	const all: Array<Post> = await fetch('https://jsonplaceholder.typicode.com/posts').then((r) =>
-		r.json()
-	);
-	const start = (page - 1) * perPage + 1;
-	return all.slice(start, Math.min(start + perPage, all.length));
+  const all: Array<Post> = await fetch('https://jsonplaceholder.typicode.com/posts').then((r) =>
+    r.json()
+  );
+  const start = (page - 1) * perPage + 1;
+  return all.slice(start, Math.min(start + perPage, all.length));
 }
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,32 +1,32 @@
 <script context="module" lang="ts">
-	import { getPostStats } from '$lib/api';
-	import type { LoadInput } from '@sveltejs/kit';
+  import { getPostStats } from '$lib/api';
+  import type { LoadInput } from '@sveltejs/kit';
 
-	export async function load({ fetch }: LoadInput) {
-		const { maxPages } = await getPostStats(fetch);
-		return {
-			props: {
-				maxPages
-			}
-		};
-	}
+  export async function load({ fetch }: LoadInput) {
+    const { maxPages } = await getPostStats(fetch);
+    return {
+      props: {
+        maxPages
+      }
+    };
+  }
 </script>
 
 <script lang="ts">
-	export let maxPages: number;
+  export let maxPages: number;
 
-	const pages = [...Array(maxPages).keys()].map((p) => p + 1);
+  const pages = [...Array(maxPages).keys()].map((p) => p + 1);
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 
 <p>
-	This is an example application that demenstrate how to have a SvelteKit application with
-	prerendered pages with pagaination support.
+  This is an example application that demenstrate how to have a SvelteKit application with
+  prerendered pages with pagaination support.
 </p>
 
 <ul>
-	{#each pages as page}
-		<li><a href={`/post/${page}`}>Page {page}</a></li>
-	{/each}
+  {#each pages as page}
+    <li><a href={`/post/${page}`}>Page {page}</a></li>
+  {/each}
 </ul>
